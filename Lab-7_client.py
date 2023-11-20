3b816bf8be9149ca977afb3eb09dba5a60fb50b5
Dev <dev.ace21@sot.pdpu.ac.in>
2023-11-21 02:13:13 +0530

    command

--- Lab-7_client.py
+++ Lab-7_client.py
@@ -1,7 +1,8 @@
 import socket
+
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c.connect(('127.0.0.1', 1111))
-name = input('enter name: ')
-c.send(bytes(name, 'utf-8'))
+c.connect(("127.0.0.1", 1111))
+name = input("enter name: ")
+c.send(bytes(name, "utf-8"))
 print(c.recv(1024).decode())
 c.close()
--- Lab-7_server.py
+++ Lab-7_server.py
@@ -1,14 +1,14 @@
 import socket
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#create socket object
 print("socket created?")
-s.bind(('127.0.0.1', 1111)) #bind to port
+s.bind(("127.0.0.1", 1111))
 s.listen(5)
 print("waiting for connection")
 while True:
     c, addr = s.accept()
     name = c.recv(1024).decode
-    print ('got connection from ', addr)
-    c.send(bytes("connection done", 'UTF-8'))
+    print("got connection from ", addr)
+    c.send(bytes("connection done", "UTF-8"))
     c.close()
     break
